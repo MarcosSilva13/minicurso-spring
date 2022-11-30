d9854a73f2136c26ca7a07469ab0207b1109dc4e
MARCOS ANTONIO DA SILVA <0057607@IFMG.EDU.BR>
2022-11-30 16:38:11 -0300

    iniciando projeto

---
+++ src/main/java/com/ifmg/usuarios/controller/SalvarUsuarioController.java
@@ -0,0 +1,24 @@
+package com.ifmg.usuarios.controller;
+
+import com.ifmg.usuarios.dto.UsuarioDto;
+import com.ifmg.usuarios.service.SalvarUsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SalvarUsuarioController {
+
+    @Autowired
+    private SalvarUsuarioService salvarUsuarioService;
+
+    @PostMapping
+    public ResponseEntity<Object> salvarUsuario(@RequestBody UsuarioDto usuarioDto){
+        salvarUsuarioService.salvaUsuario(usuarioDto.getNome(),
+                usuarioDto.getCpf(), usuarioDto.getDataDeNascimento());
+
+        return ResponseEntity.ok().body("Usuario foi salvo no banco!");
+    }
+}
